714b9253ea8c6f1d57706cfb5c713a7dc7778ea9
kurowskicisa <kurowskic.isa@gmail.com>
2018-06-08 20:13:37 +0200

    20180608

---
+++ cwiczenie002/src/cwiczenie002.java
@@ -0,0 +1,10 @@
+public class cwiczenie002 {
+    public static void main(String[] args){
+        System.out.println(add(5,4))
+    }
+
+    init add(int a, int b) {
+        int sum;
+        sum = a+b;
+    }
+}
