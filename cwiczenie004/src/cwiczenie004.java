e04ccabdd0edcb49827b5c2e33ec322562150250
kurowskicisa <kurowskic.isa@gmail.com>
2018-06-08 21:55:30 +0200

    20180608

--- cwiczenie004/src/cwiczenie004.java
+++ cwiczenie004/src/cwiczenie004.java
@@ -1,4 +1,20 @@
 public class cwiczenie004 {
-    
+    public static void main (String[] args){
+        System.out.println( firstLetterofStting ("Imię i nazwisko"));
+        System.out.println( firstLetterofStting ("Adam"));
+        System.out.println( firstLetterofStting ("Ewa"));
+    }
+
+
+    public static String firstLetterofStting (String text) {
+        char fl;
+
+
+        fl = text.charAt(0);
+
+        return text + " - " + fl;
+
+    }
+
 
 }
